d45bf2279fdd4d0279d49c526496ed3e9a83e771
benedekaibas <kaibas.benedek02@gmail.com>
2025-08-03 14:50:39 +0200

    edit: file updated with learning material

---
+++ src/Constants/constants.cpp
@@ -0,0 +1,12 @@
+// DESIGN GOALS OF C++
+
+/*
+By understanding the goals of C++, we can understand why we use constants in the language.
+The goals of C++ described by Stroustrup:
+    Type safety
+    Resource safety
+    Performance control
+    Predictability
+    Readability
+    Learnability
+*/
