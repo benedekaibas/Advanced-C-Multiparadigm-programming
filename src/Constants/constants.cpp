96f06910c0406ed843f408c3f5f9e1f64b8d4cc9
benedekaibas <kaibas.benedek02@gmail.com>
2025-08-22 11:36:10 +0200

    docs: new files & content

--- src/Constants/constants.cpp
+++ src/Constants/constants.cpp
@@ -26,7 +26,25 @@ int main()
     return 0; /* not strictly necessary since C99 */
 }
 
+// C++ example
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    std::ifstream f;
+    . . .
+    f << "Hello input!" << std::endl;
+    return 0;
+}
+
 /*
 In C we would get a runtime error since we open the file in reading mode, but we want to write on it.
 In C++ it would be a compiler error since as we know the ifstream does not have insertion operator.
+
+In C++ it's important to focus on detecting errors that happen in compilation time.
 */
+
+
+
+// SIDE NOTE: SCWARTZ ERROR
